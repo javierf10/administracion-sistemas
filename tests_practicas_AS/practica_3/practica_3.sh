08415ec97724bdf8ac60691fd958a1544eb6e31a
javierf10 <157632748+javierf10@users.noreply.github.com>
2024-03-25 19:31:53 +0100

    Update practica_3.sh

--- tests_practicas_AS/practica_3/practica_3.sh
+++ tests_practicas_AS/practica_3/practica_3.sh
@@ -2,68 +2,72 @@
 #873879, Alonso, Jaime, T, 1, B
 #797613, Falcó, Javier, T, 1, B
 
-#Fución que comprueba si el usuario tiene privilegios de administrador
-check_admin(){
+
+# Script para la gestión de usuarios
+
+# Función que comprueba si el usuario tiene privilegios de administrador
+verificar_administrador(){
 	if [ "$EUID" -ne 0 ]; then
-		echo "Este script necesita privilegios de administracion" >&2
+		echo "Este script necesita privilegios de administración" >&2
 		exit 1
 	fi
 }
 
 # Función que crea un usuario y establece su contraseña
-create_user() {
-  local username="$1"
-  local password="$2"
-  local fullname="$3"
+crear_usuario() {
+  local nombre_usuario="$1"
+  local contrasegna="$2"
+  local nombre_completo="$3"
 
-  if id -u "$username" >/dev/null 2>&1; then
-    echo "El usuario $username ya existe"
+  # Verificar si el usuario ya existe
+  if id -u "$nombre_usuario" >/dev/null 2>&1; then
+    echo "El usuario $nombre_usuario ya existe"
     return
   fi
 
-  if [[ -z "$username" || -z "$password" || -z "$fullname" ]]; then
+  # Verificar si algún campo está vacío
+  if [[ -z "$nombre_usuario" || -z "$contrasegna" || -z "$nombre_completo" ]]; then
     echo "Campo invalido"
     return
   fi
 
-  #local uid="$(echo $RANDOM+1815 | bc)"
-  local homedir="/home/$username"
+  local directorio_personal="/home/$nombre_usuario"
 
-  # Creamos el usuario con el UID generado y su grupo con el mismo nombre
-  useradd -u UID_MIN=1815 -d "$homedir" -m -s /bin/bash "$username"
+  # Creamos el usuario con su directorio personal y grupo
+  useradd -u UID_MIN=1815 -d "$directorio_personal" -m -s /bin/bash "$nombre_usuario"
   # Establecemos su contraseña
-  echo "$username:$password" | chpasswd
+  echo "$nombre_usuario:$contrasegna" | chpasswd
 
   # Establecemos la caducidad de la contraseña a 30 días
-  chage -d 0 -M 30 "$username"
+  chage -d 0 -M 30 "$nombre_usuario"
 
   # Copiamos los archivos de /etc/skel al directorio home del usuario
-  cp -r /etc/skel/. "$homedir"
+  cp -r /etc/skel/. "$directorio_personal"
 
-  echo "$fullname ha sido creado"
+  echo "$nombre_completo ha sido creado"
 }
 
 # Función que borra un usuario y realiza un backup de su directorio home
-delete_user() {
-  local username="$1"
+borrar_usuario() {
+  local nombre_usuario="$1"
 
-  local homedir="/home/$username"
-  local backupdir="/extra/backup"
+  local directorio_personal="/home/$nombre_usuario"
+  local directorio_backup="/extra/backup"
 
   # Creamos el directorio de backup si no existe
-  if [ ! -d "$backupdir" ]; then
-    mkdir -p "$backupdir"
+  if [ ! -d "$directorio_backup" ]; then
+    mkdir -p "$directorio_backup"
   fi
 
   # Realizamos el backup del directorio home del usuario
-  tar -cf "$backupdir/$username.tar" "$homedir"
+  tar -cf "$directorio_backup/$nombre_usuario.tar" "$directorio_personal"
 
   # Borramos al usuario y su directorio home
-  userdel -r "$username"
+  userdel -r "$nombre_usuario"
 }
 
 # Comprobamos si el usuario actual tiene privilegios de administración
-check_admin
+verificar_administrador
 
 # Comprobamos si se han especificado los argumentos correctamente
 if [ "$#" -ne 2 ]; then
@@ -74,16 +78,16 @@ fi
 # Procesamos el fichero de entrada
 case "$1" in
   -a)
-    while IFS=',' read -r username password fullname; do
-      create_user "$username" "$password" "$fullname"
+    while IFS=',' read -r nombre_usuario contrasegna nombre_completo; do
+      crear_usuario "$nombre_usuario" "$contrasegna" "$nombre_completo"
     done < "$2"
     ;;
   -s)
     # Creamos el directorio de backup si no existe
     mkdir -p /extra/backup
 
-    while IFS=',' read -r username _ _; do
-      delete_user "$username"
+    while IFS=',' read -r nombre_usuario _ _; do
+      borrar_usuario "$nombre_usuario"
     done < "$2"
     ;;
   *)
