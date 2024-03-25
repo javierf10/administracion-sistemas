659e137ed3a095e4b5cfcda2ccb6a33181e643ed
javierf10 <157632748+javierf10@users.noreply.github.com>
2024-03-25 19:01:12 +0100

    Update practica_3.sh

--- tests_practicas_AS/practica_3/practica_3.sh
+++ tests_practicas_AS/practica_3/practica_3.sh
@@ -1,96 +1,63 @@
 #!/bin/bash
-#873879, Alonso, Jaime, T, 1, B
-#797613, Falcó, Javier, T, 1, B
 
-# script que permita añadir y suprimir un conjunto de usuarios
-# especificados en un fichero
+# Script para añadir o suprimir usuarios especificados en un archivo.
 
-
-#Permisos de administrador
-if [ "$EUID" -ne 0 ]
-then
-	echo "Este script necesita privilegios de administracion"
-	exit 1
+# Comprobamos si se tienen privilegios de administrador.
+if [ "$EUID" -ne 0 ]; then
+    echo "Este script requiere privilegios de administrador."
+    exit 1
 fi
 
-#Comprobamos numero de argumentos
-if [ "$#" -eq 2 ]
-then
-	if [ "$1" = "-a" ] #Parametro 1 == '-a'-> añadir usuario
-	then
+# Comprobamos el número de argumentos.
+if [ "$#" -eq 2 ]; then
+    if [ "$1" = "-a" ]; then  # Opción '-a': Añadir usuario.
 
-		#Se añade un usuario
-		OLDIFS=$IFS
-		IFS=,
-		while read -r usuario password nombre #Leemos usuarios,contraseñas y nombres del fichero
-		do
-			#Comprobamos que los campos leidos con distintos de " " (vacio)
-			if [ -z "$usuario" ]
-			then
-				echo "Campo invalido"
-				IFS=$OLDIFS
-				exit 1
-			elif [ -z "$password" ]
-			then
-				echo "Campo invalido"
-				IFS=$OLDIFS
-				exit 1
-			elif [ -z "$nombre" ]
-			then
-				echo "Campo invalido"
-				IFS=$OLDIFS
-				exit 1
-			fi
-			#Añadimos un nuevo usuario
-			#-U -> creamos el grupo con el mismo nombre que el usuario.
-			#-k /etc/skel -> decimos cual será el skeleton directory (directorio del que se copiarán los ficheros y directorios al home)
-			#-K UID_MIN=1815 -> hacemos que el UID sea >=1815
-			#-K PASS_MAX_DAYS=30 -> decimos que el número máximo de días que se `puede usar la contraseña será 30.
-			#-c "$nombre" "$usuario" -> pondrá el nombre y usuario completo.
-			useradd -U -m -k /etc/skel -K UID_MIN=1815 -K PASS_MAX_DAYS=30 -c "$nombre" "$usuario" 2>/dev/null
+        # Añadimos usuarios del archivo especificado.
+        DELIMITER=$IFS  # Guardamos el delimitador original.
+        IFS=','  # Separador de campos.
+        while read -r username password fullname; do
+            # Verificamos que los campos no estén vacíos.
+            if [ -z "$username" ] || [ -z "$password" ] || [ -z "$fullname" ]; then
+                echo "Campo inválido."
+                IFS=$DELIMITER  # Restauramos el delimitador original.
+                exit 1
+            fi
+            # Creamos un nuevo usuario.
+            useradd -U -m -k /etc/skel -K UID_MIN=1815 -K PASS_MAX_DAYS=30 -c "$fullname" "$username" 2>/dev/null
 
-			#Comprobamos si existe el usuario
-			if [ "$?" -ne 0 ]
-			then
-				echo "El usuario $usuario ya existe"
-			else
-				echo "${usuario}:${password}" | chpasswd #Actualizamos la contraseña del usuario
-				echo "${nombre} ha sido creado"
-				usermod -aG 'sudo' ${usuario} #Añadimos el usuario
-			fi
-		done < $2 #$2=ficheroUsuarios
-		IFS=$OLDIFS
+            # Verificamos si el usuario ya existe.
+            if [ "$?" -ne 0 ]; then
+                echo "El usuario $username ya existe."
+            else
+                echo "${username}:${password}" | chpasswd  # Actualizamos la contraseña.
+                echo "${fullname} ha sido creado."
+                usermod -aG 'sudo' ${username}  # Añadimos el usuario al grupo 'sudo'.
+            fi
+        done < "$2"  # Leemos desde el archivo especificado.
+        IFS=$DELIMITER  # Restauramos el delimitador original.
 
-	elif [ "$1" = "-s" ] #Parámetro 1 == '-s' -> borrar usuario
-	then
-		#Se borra un usuario
-		#Se crea directorio extra/backup
-		if [ ! -d /extra ]
-		then
-			mkdir -p /extra/backup
-		elif [ ! -d /extra/backup ]
-		then
-			mkdir /extra/backup
-		fi
-		OLDIFS=$IFS
-		IFS=,
-		#Para borrar usuarios solo es necesario el primer campo
-		while read -r usuario password nombre #Leemos usuario, contraseñas y nombres del fichero
-		do
-			user_home="$(getent passwd ${usuario} | cut -d: -f6)" #Guarda el directorio home del usuario ${usuario}
-			tar cvf "/extra/backup/${usuario}.tar" "$user_home" &>/dev/null #Hacemos backup del directorio home
-			if [ "$?" -eq 0 ]
-			then
-				userdel -f "$usuario" &>/dev/null #borramos el usuario
-			fi
-		done < $2 #$2=ficheroUsuarios
-		IFS=$OLDIFS
-	else
-		#Ni añadir usuario(-a) ni borrar usuario (-s)
-		echo "Opcion invalida"
-	fi
+    elif [ "$1" = "-s" ]; then  # Opción '-s': Borrar usuario.
+
+        # Creamos el directorio extra/backup si no existe.
+        if [ ! -d /extra ]; then
+            mkdir -p /extra/backup
+        elif [ ! -d /extra/backup ]; then
+            mkdir /extra/backup
+        fi
+        DELIMITER=$IFS  # Guardamos el delimitador original.
+        IFS=','  # Separador de campos.
+        while read -r username password fullname; do
+            user_home="$(getent passwd ${username} | cut -d: -f6)"  # Directorio home del usuario.
+            tar cvf "/extra/backup/${username}.tar" "$user_home" &>/dev/null  # Realizamos backup del directorio home.
+            if [ "$?" -eq 0 ]; then
+                userdel -f "$username" &>/dev/null  # Borramos el usuario.
+            fi
+        done < "$2"  # Leemos desde el archivo especificado.
+        IFS=$DELIMITER  # Restauramos el delimitador original.
+
+    else
+        echo "Opción inválida."
+    fi
 else
-	echo "Numero incorrecto de parametros"
+    echo "Número incorrecto de parámetros."
 fi
-
-
