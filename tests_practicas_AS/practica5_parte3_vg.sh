007a54e0b076b9d9ec69a869c6a18407ea9d45c8
Jaime-4 <120720703+Jaime-4@users.noreply.github.com>
2024-05-22 18:17:26 +0200

    Add files via upload

---
+++ tests_practicas_AS/practica5_parte3_vg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#873879, Alonso, Jaime, T, 1, B
+#797613, Falcó, Javier, T, 1, B
+
+# Comprobar que el numero de argumentos es correcto
+if [ $# -lt 2 ]; then
+    echo "Sintaxis: practica5_parte3_vg.sh <grupo_volumen> <particion_1> ..."
+    exit
+fi
+
+# Comprobar si el usuario actual tiene privilegios de administración
+if [ "$EUID" -ne 0 ]; then
+    echo "Este script necesita privilegios de administración" >&2
+    exit 1
+fi
+
+gv=$1
+
+if [ "$gv" != "vg_p5"]
+then
+    echo "El grupo volumen no es el correcto" >&2
+    exit 2
+fi
+shift
+
+# Bucle que recorre todas las particiones, desmontándolas primero y luego extendiéndolas
+for particion in "$@"
+do
+    unmount "$particion" # Desmontar la particion primero
+    vgextend "$vg" "$particion" # Extender la particion
+done
